1d3d9f330e9254968e3cf2cdaf9853b2dc16c066
Manu <black_falcon00@hotmail.com>
2018-07-08 18:02:24 +0200

    Debut, murs et 1 raquette en place, gestion du movement. Collisions ... pas top top.

---
+++ Assets/Scripts/RacketMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketMovement : MonoBehaviour {
+
+	public int m_PlayerNumber = 1;         
+	public float m_Speed = 12f;            
+
+	private string m_MovementAxisName;
+	private float m_MovementInputValue;
+	private Rigidbody m_Rigidbody;         
+
+	private void Awake()
+	{
+		m_Rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private void Start()
+	{
+		m_MovementAxisName = "Vertical" + m_PlayerNumber;
+	}
+
+	private void Update()
+	{
+		m_MovementInputValue =Input.GetAxis(m_MovementAxisName);
+	}
+
+	private void FixedUpdate()
+	{
+		Move ();
+	}
+
+	private void Move()
+	{
+		// Adjust the position of the tank based on the player's input.
+		Vector3 movement = transform.up * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+		m_Rigidbody.MovePosition (m_Rigidbody.position + movement);
+	}
+}
